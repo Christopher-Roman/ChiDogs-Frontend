e71879a31671ccab5f6a0213dd9572ec761bf7e7
Chris Roman <Christopherroman85@gmail.com>
2018-12-05 20:45:50 -0600

    added the s

--- src/apiURL.js
+++ src/apiURL.js
@@ -4,7 +4,7 @@
 let apiUrl;
 
 if(Object.keys(process.env).findIndex(key => key=='REACT_APP_LOCAL_VERSION') == -1){
-	apiUrl = 'http://chidogs.herokuapp.com'
+	apiUrl = 'https://chidogs.herokuapp.com'
 } else {
 	apiUrl = 'http://localhost:8000'
 }
